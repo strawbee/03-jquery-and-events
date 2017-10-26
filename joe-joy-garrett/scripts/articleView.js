857775461433dbf2d5d61f8805a2919813e3dfaa
Joe Waine <joseph@comradeagency.com>
2017-10-26 10:30:14 -0700

    finishing articleView.js - added stretch goal

--- joe-joy-garrett/scripts/articleView.js
+++ joe-joy-garrett/scripts/articleView.js
@@ -85,14 +85,25 @@ articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // DONE: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  $('.read-on').on('click', function(e){
+    e.preventDefault();
+    $(this).parent().find('.article-body *:nth-of-type(n+2)').toggle();
+    $(this).toggleClass('expanded');
+    $('.read-on').text('Read more →');
+    $('.read-on.expanded').text('← Read less');
+  });
+
+
+
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
+// DONE: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
   articleView.handleMainNav();
+  articleView.setTeasers();
 })
